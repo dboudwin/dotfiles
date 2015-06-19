cb2f5c5e024e05ee7cc7ea1994f7b76fe11952fa
Derrick Boudwin <derrick.boudwin@gmail.com>
2015-06-19 08:09:58 -0600

    add link creator

---
+++ link.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Links your repo with your local profile
+
+ln -s "$(pwd)/.bash_profile" ~/.bash_profile
+ln -s "$(pwd)/.bash_functions" ~/.bash_functions
+ln -s "$(pwd)/.bashrc" ~/.bashrc
